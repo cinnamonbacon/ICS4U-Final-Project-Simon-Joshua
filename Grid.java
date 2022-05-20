a4e6b6af8c675323c30637ff9d6939205b924900
TheGreatestGitUser <joshua123033@gmail.com>
2022-05-20 19:09:21 -0400

    Added JavaDoc comments to Grid and Tile
    Added more JavaDoc comments Grid.java and Tile.java programs.

--- Grid.java
+++ Grid.java
@@ -3,20 +3,44 @@
   Teacher: Ms. Krasteva
   Date: May 20, 2022
   Course: ICS4U0/P
-  Purpose: To build manipulate and draw a screen with tiles that the player can move around in.
+  
+  Purpose: to build manipulate and draw a screen with tiles that the player can move around in.
   Worked on by: Fully Simon
 */
 
-import javafx.stage.*;
-import javafx.scene.layout.*;
-import javafx.scene.Scene;
-import javafx.scene.Group;
-import java.util.*;
-import java.io.*;
-import java.awt.image.*;
-import javafx.scene.image.*;
-import javax.imageio.*;
-import javafx.embed.swing.*;
+import javafx.stage.Stage; // Imports the Stage.java class, which allows the program to have access to a stage to display the graphics within.
+import javafx.scene.layout.GridPane; // Imports the GridPane.java class, which allows the program to have access to a grid layout to easily organize Nodes and other graphics within.
+import javafx.scene.layout.VBox; // Imports the VBox.java class, which allows the program to have access to the vertical box layout to easily organize Nodes and other graphics.
+import javafx.scene.layout.StackPane; // Imports the StackPane.java class, which allows the program to have access to the stack layout to easily organize Nodes and other graphis within (it can now stack the panes).
+import javafx.scene.Scene; // Imports the Scene.java class, which allows the program to have access to a scene on which the GUI can be added onto and displayed to the user.
+import javafx.scene.Group; // Imports the Group.java class, which allows the program to have access to the group layout to easily organize Nodes and other graphics within (it can group various nodes together).
+import java.io.File; // Imports the File.java class, which allows the program to easily access files that are on the user's computer.
+import javafx.scene.image.Image; // Imports the Image.java class, which allows the program to easily create images from various files that are on the user's computer.
+import javafx.scene.image.ImageView; // Imports the ImageView.java class, which allows the program to easily create images that can act as nodes and can be added onto the scenes (and displayed to the user).
+
+/**
+ * Create a Grid made of Tiles that the user will move around in.
+ * <p>
+ * This class will create a grid made from numerous instances of
+ * the Tile.java class, which will all be stored within a global
+ * 2D array that will be a code representation of the grid. This
+ * class will also have various methods that help with user
+ * movement.
+ * <p>
+ * Course: ICS 4U0/P
+ * <p>
+ * Teacher Name: Ms. Krasteva
+ * <p>
+ * Purpose: to build manipulate and draw a screen with tiles
+ * that the player can move around in.
+ * <p>
+ * Filename: Grid.java
+ *
+ * @author Simon Bakan
+ * @version 5/20/2022
+ *
+ * @since JDK1.17
+ */
 
 public class Grid{
 
@@ -30,9 +54,9 @@ public class Grid{
    private int mainY;
    
    /**
-     * An instance of the Tile class will be created using this constructor with
-     * parameters for the x and y coordinate.
-     */
+    * An instance of the Tile class will be created using this constructor with
+    * parameters for the x and y coordinate.
+    */
    public Grid(int x, int y){
       tiles = new Tile[20][20];
       this.mainX=x;
@@ -40,33 +64,33 @@ public class Grid{
    }
    
    /**
-     * Public non-static return method used instantiate tiles.
-     * <p>
-     * This private non-static method will be used instantiate tiles in
-     * the grid with specific attributes.
-     *
-     * @param x An int with the x coordinate of the tile.
-     * @param y An int with the y coordinate of the tile.
-     * @param file A String with the pathway for the image for the tile.
-     * @param object A String with the pathway for the image for the object
-     *               that is on the tile.
-     * @param movable A boolean with the attribute of whether the player will
-     *                be able to move onto this tile.
-     * @param movable A boolean with the attribute of whether the player will
-     *                be able to interact with this tile.
-     */
+    * Public non-static return method used instantiate tiles.
+    * <p>
+    * This private non-static method will be used instantiate tiles in
+    * the grid with specific attributes.
+    *
+    * @param x An int with the x coordinate of the tile.
+    * @param y An int with the y coordinate of the tile.
+    * @param file A String with the pathway for the image for the tile.
+    * @param object A String with the pathway for the image for the object
+    *               that is on the tile.
+    * @param movable A boolean with the attribute of whether the player will
+    *                be able to move onto this tile.
+    * @param movable A boolean with the attribute of whether the player will
+    *                be able to interact with this tile.
+    */
    public void assign(int x, int y, String file, String object, boolean movable, boolean interactable){
       tiles[y][x] = new Tile(file,object,movable,interactable);
    }
    
    /**
-     * Public non-static return method used to move the player left.
-     * <p>
-     * This private non-static return method will be used to check if the player
-     * can move left and then move them left if they can.
-     *
-     * @return A boolean that will say whether the player has walked off of the screen.
-     */
+    * Public non-static return method used to move the player left.
+    * <p>
+    * This private non-static return method will be used to check if the player
+    * can move left and then move them left if they can.
+    *
+    * @return A boolean that will say whether the player has walked off of the screen.
+    */
    public boolean moveLeft(){
       if(mainX==0){
          return true;
@@ -78,13 +102,13 @@ public class Grid{
    }
    
    /**
-     * Public non-static return method used to move the player right.
-     * <p>
-     * This private non-static return method will be used to check if the player
-     * can move left and then move them right if they can.
-     *
-     * @return A boolean that will say whether the player has walked off of the screen.
-     */
+    * Public non-static return method used to move the player right.
+    * <p>
+    * This private non-static return method will be used to check if the player
+    * can move left and then move them right if they can.
+    *
+    * @return A boolean that will say whether the player has walked off of the screen.
+    */
    public boolean moveRight(){
       if(mainX==tiles[0].length){
          return true;
@@ -96,13 +120,13 @@ public class Grid{
    }
    
    /**
-     * Public non-static return method used to move the player up.
-     * <p>
-     * This private non-static return method will be used to check if the player
-     * can move left and then move them up if they can.
-     *
-     * @return A boolean that will say whether the player has walked off of the screen.
-     */
+    * Public non-static return method used to move the player up.
+    * <p>
+    * This private non-static return method will be used to check if the player
+    * can move left and then move them up if they can.
+    *
+    * @return A boolean that will say whether the player has walked off of the screen.
+    */
    public boolean moveUp(){
       if(mainY==0){
          return true;
@@ -114,13 +138,13 @@ public class Grid{
    }
    
    /**
-     * Public non-static return method used to move the player down.
-     * <p>
-     * This private non-static return method will be used to check if the player
-     * can move left and then move them down if they can.
-     *
-     * @return A boolean that will say whether the player has walked off of the screen.
-     */
+    * Public non-static return method used to move the player down.
+    * <p>
+    * This private non-static return method will be used to check if the player
+    * can move left and then move them down if they can.
+    *
+    * @return A boolean that will say whether the player has walked off of the screen.
+    */
    public boolean moveDown(){
       if(mainY==tiles.length){
          return true;
@@ -132,14 +156,14 @@ public class Grid{
    }
    
    /**
-     * Public non-static method used to draw the grid on the passed scene.
-     * <p>
-     * This private non-static method will be used draw a grid of tiles and
-     * then their given objects followed by an image for the playeron a scene
-     * and set the stage to be that scene.
-     *
-     * @param st The Stage that the grid will be drawn on.
-     */
+    * Public non-static method used to draw the grid on the passed scene.
+    * <p>
+    * This private non-static method will be used draw a grid of tiles and
+    * then their given objects followed by an image for the playeron a scene
+    * and set the stage to be that scene.
+    *
+    * @param st The Stage that the grid will be drawn on.
+    */
    public void draw(Stage st){
       GridPane screen =  new GridPane();
       for(int row=0;row<tiles.length;row++){
@@ -188,14 +212,14 @@ public class Grid{
    }
    
    /**
-     * Public non-static method used change interactable for tiles.
-     * <p>
-     * This private non-static method will be make the boolean interactable
-     * true for the all tiles with a certain object on them.
-     *
-     * @param obj A String with the object the program is looking for to make
-     *           tiles interactable.
-     */
+    * Public non-static method used change interactable for tiles.
+    * <p>
+    * This private non-static method will be make the boolean interactable
+    * true for the all tiles with a certain object on them.
+    *
+    * @param obj A String with the object the program is looking for to make
+    *           tiles interactable.
+    */
    public void makeInteractable(String obj){
       for(int row=0;row<tiles.length;row++){
          for(int col=0;col<tiles[0].length;col++){
@@ -205,4 +229,4 @@ public class Grid{
          }
       }
    }
-}
+} // End of the Grid.java class
--- Tile.java
+++ Tile.java
@@ -3,10 +3,33 @@
   Teacher: Ms. Krasteva
   Date: May 20, 2022
   Course: ICS4U0/P
-  Purpose: To act as a variable type for a tile in the graphics of the game.
+  Purpose: to act as a variable type for a tile in the graphics of the game.
   Worked on by: Fully Simon
 */
 
+/**
+ * Create a Tile datatype that will represent the tiles in the game's graphics.
+ * <p>
+ * This class will be used to create tiles that will be on the
+ * grid of the game. Each tile will have a certain image on it,
+ * to represent the different textures (grass, sand, water, etc).
+ * It may also have a object on it that the user can interact
+ * with (such as a book or a person).
+ * <p>
+ * Course: ICS 4U0/P
+ * <p>
+ * Teacher Name: Ms. Krasteva
+ * <p>
+ * Purpose: to act as a variable type for a tile in the graphics
+ * of the game.
+ * <p>
+ * Filename: Tile.java
+ *
+ * @author Simon Bakan
+ * @version 5/20/2022
+ *
+ * @since JDK1.17
+ */
 public class Tile{
    /** This private non-static String will hold the pathway of the image file for the tile. */
    private String file;
@@ -21,8 +44,8 @@ public class Tile{
    private boolean interactable;
    
    /**
-     * An instance of the Tile class will be created using this constructor.
-     */
+    * An instance of the Tile class will be created using this constructor.
+    */
    public Tile(String file, String object, boolean movable, boolean interactable){
       this.file=file;
       this.object=object;
@@ -32,62 +55,62 @@ public class Tile{
    }
    
    /**
-     * Public non-static return method used get the value of movable.
-     * <p>
-     * This private non-static return method will be used as an accesor method
-     * for the movable attribute of the tile.
-     *
-     * @return  A boolean that will be the status of the movable boolean.
-     */
+    * Private non-static return method used get the value of movable.
+    * <p>
+    * This private non-static return method will be used as an accesor method
+    * for the movable attribute of the tile.
+    *
+    * @return  A boolean, which is the status of the movable boolean.
+    */
    public boolean canMove(){
       return movable;
    }
    
    /**
-     * Public non-static return method used get the value of interactable.
-     * <p>
-     * This private non-static return method will be used as an accessor method 
-     * for the interactable attribute of the tile.
-     *
-     * @return  A boolean that will be the status of the interactable boolean.
-     */
+    * Private non-static return method used get the value of interactable.
+    * <p>
+    * This private non-static return method will be used as an accessor method 
+    * for the interactable attribute of the tile.
+    *
+    * @return  A boolean, which is the status of the interactable boolean.
+    */
    public boolean isInteractable(){
       return interactable;
    }
    
    /**
-     * Public non-static method used change the value of interactable.
-     * <p>
-     * This private non-static method to act as a accessor method allowing other 
-     * classes to make a tile interactable.
-     */
+    * Private non-static return method used change the value of interactable.
+    * <p>
+    * This private non-static method to act as a accessor method allowing other 
+    * classes to make a tile interactable.
+    */
    public void makeInteractable(){
       interactable=true;
    }
    
    /**
-     * Public non-static return method used get the value of file.
-     * <p>
-     * This private non-static return method will be used as an accessor method 
-     * for the file attribute of the tile which has the pathway for the image.
-     *
-     * @return  A String that will be the pathway for the image for the tile.
-     */
+    * Private non-static return method used get the value of file.
+    * <p>
+    * This private non-static return method will be used as an accessor method 
+    * for the file attribute of the tile which has the pathway for the image.
+    *
+    * @return  A String, which is the pathway for the image for the tile.
+    */
    public String getFile(){
       return file;
    }
    
    /**
-     * Public non-static return method used get the value of object.
-     * <p>
-     * This private non-static return method will be used as an accessor method 
-     * for the object attribute of the tile which has the pathway for the image
-     * for the object on the tile.
-     *
-     * @return  A String that will be the pathway for the image for the object
-     * on the tile.
-     */
+    * Private non-static return method used get the value of object.
+    * <p>
+    * This private non-static return method will be used as an accessor method 
+    * for the object attribute of the tile which has the pathway for the image
+    * for the object on the tile.
+    *
+    * @return  A String, which is the pathway for the image for the object
+    * on the tile.
+    */
    public String getObject(){
       return object;
    }
-}
+} // End of the Tile.java class
