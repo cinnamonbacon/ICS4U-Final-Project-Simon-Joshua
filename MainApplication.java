29ee960f5ffd572c39a2911cf97e2bab64f5268e
TheGreatestGitUser <joshua123033@gmail.com>
2022-05-20 07:48:32 -0400

    Updated MainApplication, Added new image
    Updated the MainApplication to have some code for taking in input from the mouse. Also, an image has been added for the "New Game" button in the main menu.

--- MainApplication.java
+++ MainApplication.java
@@ -16,19 +16,23 @@ import javafx.scene.text.*;
 import javafx.scene.shape.*;
 import javafx.scene.paint.*;
 import javafx.scene.image.*;
+import javafx.scene.input.*;
 import javafx.stage.*;
 import javafx.util.*;
+import javafx.concurrent.*;
 
 import java.io.*;
 import java.util.*;
 import java.math.*;
 import java.lang.*;
 //import java.awt.*;
+import java.awt.Point;
+import java.awt.MouseInfo;
 import javax.swing.*;
 //import java.time.*;
 import java.util.concurrent.*;
 
-// Number of things to change: 1
+// Number of things to change: 2
 
 public class MainApplication extends Application {
    private File logoFile;
@@ -43,6 +47,8 @@ public class MainApplication extends Application {
    
    private File additionalGrassTileFile;
    
+   private File newGameFile;
+   
    public MainApplication () {
       logoFile = new File("ICS ISP - Single Star Games Logo.png");
       introBorderFile = new File("ICS ISP - Border for Intro Screens.png");
@@ -50,7 +56,7 @@ public class MainApplication extends Application {
       blackTitleFile = new File("ICS ISP - Title of Game (Black Version).png");
       grassTileFile = new File("Grass.png");
       additionalGrassTileFile = new File("AdditionalGrass.png");
-      
+      newGameFile = new File("ICS ISP - Button Design for New Game Button.png");      
    }
    
    public void initializeStageSettings(Stage stage) throws IOException {
@@ -70,7 +76,7 @@ public class MainApplication extends Application {
       logoImageView.setY(100);
       logoImageView.setFitWidth(400);
       
-      FadeTransition ftLogo = new FadeTransition(Duration.millis(5000), logoImageView);
+      FadeTransition ftLogo = new FadeTransition(Duration.millis(1), logoImageView); // <-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- set this to 5000 for 5 seconds later on
       ftLogo.setFromValue(0.0);
       ftLogo.setToValue(1.0);
       ftLogo.setByValue(0.01);
@@ -115,20 +121,72 @@ public class MainApplication extends Application {
       introBorderImageView.setY(0);
       introBorderImageView.setFitWidth(600);
       
-      Rectangle blackRectangleUnderOptions = new Rectangle(50, 50, 125, 500);
+      Rectangle blackRectangleUnderOptions = new Rectangle(10, 10, 200, 580);
       blackRectangleUnderOptions.setFill(Paint.valueOf("rgb(0,0,0)"));
       
-      Rectangle greyRectangleUnderTitle = new Rectangle(50, 75, 125, 175);
-      greyRectangleUnderTitle.setFill(Paint.valueOf("rgb(128,128,128)"));
+      Rectangle greyRectangleUnderTitle = new Rectangle(10, 50, 200, 75);
+      greyRectangleUnderTitle.setFill(Paint.valueOf("rgb(96,96,96)"));
       
       ImageView whiteTitleImageView = new ImageView(new Image(whiteTitleFile.getPath()));
       whiteTitleImageView.setPreserveRatio(true);
       whiteTitleImageView.setSmooth(true);
-      whiteTitleImageView.setX(50);
-      whiteTitleImageView.setY(75);
-      whiteTitleImageView.setFitWidth(125);
+      whiteTitleImageView.setX(20);
+      whiteTitleImageView.setY(55);
+      whiteTitleImageView.setFitWidth(180);
+      
+      /*
+      stage.addEventFilter(MouseEvent.MOUSE_MOVED, e -> {
+
+        
+        final double xVal = e.getX();
+        final double yVal = e.getY();
+        
+        System.out.println(xVal + " " + yVal);
+
+      });
+      */
+      /*
+      Service<Void> service = new Service<Void>() {
+          @Override
+          protected Task<Void> createTask() {
+              return new Task<Void>() {           
+                  @Override
+                  protected Void call() throws Exception {
+                      stage.addEventFilter(MouseEvent.MOUSE_MOVED, e -> {
+                          final double xVal = e.getX();
+                          final double yVal = e.getY();
+                      
+                          System.out.println(xVal + " " + yVal);
+                      
+                      });               
+                      final CountDownLatch latch = new CountDownLatch(1);
+                      Platform.runLater(new Runnable() {                          
+                          @Override
+                          public void run() {
+                                try{
+                                    // Nothing for now
+                              }finally{
+                                  latch.countDown();
+                              }
+                          }
+                      });
+                      latch.await();                      
+                      stage.addEventFilter(MouseEvent.MOUSE_MOVED, e -> {
+                          final double xVal = e.getX();
+                          final double yVal = e.getY();
+                      
+                          System.out.println(xVal + " " + yVal);
+                      
+                      }); 
+                      return null;
+                  }
+              };
+          }
+      };
+      service.start();
+      */
            
-      Group nodesToAdd = new Group(introBorderImageView, blackRectangleUnderOptions, greyRectangleUnderTitle, whiteTitleImageView);
+      Group nodesToAdd = new Group(blackRectangleUnderOptions, greyRectangleUnderTitle, whiteTitleImageView, introBorderImageView);
      
       Scene scene = new Scene(nodesToAdd, 600, 600);
        
@@ -138,6 +196,31 @@ public class MainApplication extends Application {
       stage.show();
    
    }
+   
+   private Label createMonitoredLabel(final Label reporter) {
+    final Label monitored = new Label("Mouse Location Monitor");
+
+    monitored.setStyle("-fx-background-color: forestgreen; -fx-text-fill: white; -fx-font-size: 20px;");
+
+    monitored.setOnMouseMoved(new EventHandler<MouseEvent>() {
+      @Override public void handle(MouseEvent event) {
+        String msg =
+          "(x: "       + event.getX()      + ", y: "       + event.getY()       + ") -- " +
+          "(sceneX: "  + event.getSceneX() + ", sceneY: "  + event.getSceneY()  + ") -- " +
+          "(screenX: " + event.getScreenX()+ ", screenY: " + event.getScreenY() + ")";
+
+        reporter.setText(msg);
+      }
+    });
+
+    monitored.setOnMouseExited(new EventHandler<MouseEvent>() {
+        @Override public void handle(MouseEvent event) {
+            reporter.setText("yo");
+        }
+    });
+
+    return monitored;
+  }
 
    public void game(Stage stage) throws IOException {
       try {            
@@ -160,6 +243,7 @@ public class MainApplication extends Application {
    public void start(Stage stage) throws IOException {
       this.initializeStageSettings(stage);
       this.introAnimation(stage);
+      //MouseEvent me = new MouseEvent();
       //this.mainMenu(stage);
        
    }
