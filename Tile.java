9132a6cc0697f1ec7581b16244576f9d8a7f1577
cinnamonbacon <86315056+cinnamonbacon@users.noreply.github.com>
2022-05-20 17:10:12 -0400

    Added comments to tile
    Added Javadoc comments to the tile class.

--- Tile.java
+++ Tile.java
@@ -1,28 +1,93 @@
+/*
+  Names: Simon Bakan and Joshua Persaud
+  Teacher: Ms. Krasteva
+  Date: May 20, 2022
+  Course: ICS4U0/P
+  Purpose: To act as a variable type for a tile in the graphics of the game.
+  Worked on by: Fully Simon
+*/
+
 public class Tile{
-    private String file;
-    private String object;
-    private boolean movable;
-    private boolean interactable;
-    public Tile(String file, String object, boolean movable, boolean interactable){
-        this.file=file;
-        this.object=object;
-        this.movable=movable;
-        this.interactable=interactable;
-        
-    }
-    public boolean canMove(){
-        return movable;
-    }
-    public boolean isInteractable(){
-        return interactable;
-    }
-    public void makeInteractable(){
-        interactable=true;
-    }
-    public String getFile(){
-        return file;
-    }
-    public String getObject(){
-        return object;
-    }
+   /** This private non-static String will hold the pathway of the image file for the tile. */
+   private String file;
+   
+   /** This private non-static String will hold the pathway of the image file for the object. */
+   private String object;
+   
+   /** This private non-static boolean will hold whether the player can move to the tile. */
+   private boolean movable;
+   
+   /** This private non-static boolean will hold whether there is something to interact with on the tile. */
+   private boolean interactable;
+   
+   /**
+     * An instance of the Tile class will be created using this constructor.
+     */
+   public Tile(String file, String object, boolean movable, boolean interactable){
+      this.file=file;
+      this.object=object;
+      this.movable=movable;
+      this.interactable=interactable;
+      
+   }
+   
+   /**
+     * Private non-static return method used get the value of movable.
+     * <p>
+     * This private non-static return method will be used as an accesor method
+     * for the movable attribute of the tile.
+     *
+     * @return  A boolean that will be the status of the movable boolean.
+     */
+   public boolean canMove(){
+      return movable;
+   }
+   
+   /**
+     * Private non-static return method used get the value of interactable.
+     * <p>
+     * This private non-static return method will be used as an accessor method 
+     * for the interactable attribute of the tile.
+     *
+     * @return  A boolean that will be the status of the interactable boolean.
+     */
+   public boolean isInteractable(){
+      return interactable;
+   }
+   
+   /**
+     * Private non-static return method used change the value of interactable.
+     * <p>
+     * This private non-static method to act as a accessor method allowing other 
+     * classes to make a tile interactable.
+     */
+   public void makeInteractable(){
+      interactable=true;
+   }
+   
+   /**
+     * Private non-static return method used get the value of file.
+     * <p>
+     * This private non-static return method will be used as an accessor method 
+     * for the file attribute of the tile which has the pathway for the image.
+     *
+     * @return  A String that will be the pathway for the image for the tile.
+     */
+   public String getFile(){
+      return file;
+   }
+   
+   /**
+     * Private non-static return method used get the value of object.
+     * <p>
+     * This private non-static return method will be used as an accessor method 
+     * for the object attribute of the tile which has the pathway for the image
+     * for the object on the tile.
+     *
+     * @return  A String that will be the pathway for the image for the object
+     * on the tile.
+     */
+   public String getObject(){
+      return object;
+   }
 }
