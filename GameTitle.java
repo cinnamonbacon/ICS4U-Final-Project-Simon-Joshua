5ff8438a71d92d590b111a405785b5d85cbe935c
TheGreatestGitUser <76266641+TheGreatestGitUser@users.noreply.github.com>
2022-06-10 23:49:41 -0400

    GameTitle commenting is done

--- GameTitle.java
+++ GameTitle.java
@@ -1,42 +1,95 @@
-import javafx.scene.shape.Rectangle;
-import javafx.scene.text.Font;
-import javafx.scene.layout.StackPane;
-import javafx.scene.paint.Paint;
-import javafx.scene.control.Label;
-import javafx.scene.layout.HBox;
-import javafx.scene.Scene;
-//import javafx.geometry.Pos;
-//import javafx.scene.layout.GridPane;
-import javafx.scene.text.Text;
-import javafx.scene.text.TextAlignment;
-import javafx.scene.text.TextFlow;
-import javafx.scene.layout.Region;
+import javafx.scene.shape.Rectangle; // Imports the Rectangle.java class, which allows the program to create a rectangle that will be shown to the user (by being added as a node to the current scene).
+import javafx.scene.text.Font; // Imports the Font.java class, which allows the program to create and use fonts.
+import javafx.scene.layout.StackPane; // Imports the StackPane.java class, which allows the program to use the StackPane layout manager.
+import javafx.scene.paint.Paint; // Imports the Paint.java class, which allows the program to create and use different paint colours.
+import javafx.scene.control.Label; // Imports the Label.java class, which allows the program to use labels to compute the width and height of the given text.
+import javafx.scene.layout.HBox; // Imports the HBox.java class, which allows the program to add the labels to a layout manager so that the width and height can be computed.
+import javafx.scene.Scene; // Imports the Scene.java class, which allows the program to have access to a scene on which the GUI can be added onto and displayed to the user.
+import javafx.scene.text.Text; // Imports the Text.java class, which allows the program to take Strings of text and convert it into a good looking String for the user.
+import javafx.scene.text.TextAlignment; // Imports the TextAlignment.java class, which allows the program to set the alignment of text, especially within TextFlow instances.
+import javafx.scene.text.TextFlow; // Imports the TextFlow.java class, which allows the program to add two or more Text.java instances together.
+import javafx.scene.layout.Region; // Imports the Region.java class, which allows the program to use default computed values for the width and height.
 
-import java.io.File;
-import java.io.IOException;
-import java.io.FileInputStream;
+import java.io.File; // Imports the File.java class, which allows the program to access files that are on the user's computer.
+import java.io.IOException; // Imports the IOException.java class, which allows the program to take care of the IOExceptions that are thrown by any parts of the program.
+import java.io.FileInputStream; // Imports the FileInputStream.java class, which allows the program to read in data from files that arer on the user's computer.
 
-
+/**
+ * Helper class that will create game titles.
+ * <p>
+ * This class will be used to create a game title.
+ * It will do so by creating instances of the Text.java
+ * class and then modifying them to look like titles.
+ * 
+ * This was made fully by Joshua Persaud.
+ * <p>
+ * Course: ICS 4U0/P
+ * <p>
+ * Teacher Name: Ms. Krasteva
+ * <p>
+ * Purpose: a helper class to create game titles.
+ * <p>
+ * Filename: GameTitle.java
+ *
+ * @author Joshua Persaud
+ * @version 6/10/2022
+ * 
+ * @see javafx.scene.shape.Rectangle
+ * @see javafx.scene.text.Font
+ * @see javafx.scene.layout.StackPane
+ * @see javafx.scene.paint.Paint
+ * @see javafx.scene.control.Label
+ * @see javafx.scene.layout.HBox
+ * @see javafx.scene.Scene
+ * @see javafx.scene.text.Text
+ * @see javafx.scene.text.TextAlignment
+ * @see javafx.scene.text.TextFlow
+ * @see javafx.scene.layout.Region
+ * @see java.io.File
+ * @see java.io.IOException
+ * @see java.io.FileInputStream
+ *
+ * @since JDK1.17
+ */
 public class GameTitle {
     
+    /** Private non-static double variable used to hold the x-coordinate of the game title instance. */
     private double xCoord;
     
+    /** Private non-static double variable used to hold the y-coordinate of the game title instance. */
     private double yCoord;
     
+    /** Private non-static double variable used to hold the width of the game title instance. */
     private double width;
     
+    /** Private non-static double variable used to hold the height of the game title instance. */
     private double height;
     
+    /** Private non-static double variable used to hold the spacing of the text of the game title instance. */
     private double spacing;
     
+    /** Private non-static variable that is an instance of the Text.java class and is used to hold the title of the game title instance. */
     private Text title;
     
+    /** Private non-static variable that is an instance of the Text.java class and is used to hold the subheading of the game title instance. */    
     private Text subheading;
     
+    /** Private non-static variable that is an instance of the Rectangle.java class and is used to hold a rectangle that will act as the underline of the game title instance. */
     private Rectangle underlineRect;
     
+    /** Private non-static variable that is an instance of the File.java class and is used to hold the font file for the game title instance. */
     private File textFontFile;
     
+    /*
+     *  6 parameter constructor used to create an instance of the GameTitle.java class.
+     * 
+     * @param textFontFile An instance of the File.java class, which is the file that holds the location of the font.
+     * @param titleText An instance of the String.java class, which will hold the text for the title.
+     * @param subheadingText An instance of the String.java class, which will hold the text for the subheading.
+     * @param xCoord A double, which will be the x-coordinate of the game title instance.
+     * @param yCoord A double, which will be the y-coordinate of the game title instance.
+     * @param fontSize An integer, which will be the size of the font used in the game title instance.
+     */
     public GameTitle(File textFontFile, String titleText, String subheadingText, double xCoord, double yCoord, int fontSize) {
         this.xCoord = xCoord;
         this.yCoord = yCoord;
@@ -57,67 +110,148 @@ public class GameTitle {
         this.underlineRect.setFill(Paint.valueOf("rgb(255,255,255)"));
     }
     
+    /*
+     * Public non-static method used to get the x-coordinate of the game title instance.
+     * 
+     * @return  A double, which will be the x-coordinate of the game title instance.
+     */
     public double getXCoord() {
         return this.xCoord;
     }
     
+    /*
+     * Public non-static method used to set the x-coordinate of the game title instance.
+     * 
+     * @param newXCoord A double, which will be the new x-coordinate of the game title instance.
+     */
     public void setXCoord(double newXCoord) {
         this.xCoord = newXCoord;
     }
     
+    /*
+     * Public non-static method used to get the y-coordinate of the game title instance.
+     * 
+     * @return  A double, which will be the y-coordinate of the game title instance.
+     */
     public double getYCoord() {
         return this.yCoord;
     }
     
+    /*
+     * Public non-static method used to set the y-coordinate of the game title instance.
+     * 
+     * @param newXCoord A double, which will be the new y-coordinate of the game title instance.
+     */
     public void setYCoord(double newYCoord) {
         this.yCoord = newYCoord;
     }
     
+    /*
+     * Public non-static method used to get the width of the game title instance.
+     * 
+     * @return  A double, which will be the width of the game title instance.
+     */
     public double getWidth() {
         return this.width;
     }
     
+    /*
+     * Public non-static method used to set the width of the game title instance.
+     * 
+     * @param newWidth A double, which will be the new width of the game title instance.
+     */
     public void setWidth(double newWidth) {
         this.width = newWidth;
         this.underlineRect.setWidth(newWidth);
     }
     
+    /*
+     * Public non-static method used to get the height of the game title instance.
+     * 
+     * @return  A double, which will be the height of the game title instance.
+     */
     public double getHeight() {
         return this.height;
     }
     
+    /*
+     * Public non-static method used to set the height of the game title instance.
+     * 
+     * @param newHeight A double, which will be the new height of the game title instance.
+     */
     public void setHeight(double newHeight) {
         this.height = newHeight;
     }
     
+    /*
+     * Public non-static method used to get the spacing between the text of the game title instance.
+     * 
+     * @return  A double, which will be the spacing between the text of the game title instance.
+     */
     public double getSpacing() {
         return this.spacing;
     }
     
+    /*
+     * Public non-static method used to set the spacing between the text of the game title instance.
+     * 
+     * @param newSpacing A double, which will be the new spacing between the text of the game title instance.
+     */
     public void setSpacing(double newSpacing) {
         this.spacing = newSpacing;
     }
     
+    /*
+     * Public non-static method used to get the title text of the game title instance.
+     * 
+     * @return  An instance of the Text.java class, which will be the title text of the game title instance.
+     */
     public Text getTitleText() {
         return this.title;
     }
     
+    /*
+     * Public non-static method used to get the subheading text of the game title instance.
+     * 
+     * @return  An instance of the Text.java class, which will be the subheading text of the game title instance.
+     */
     public Text getSubheadingText() {
         return this.subheading;
     }
     
+    /*
+     * Public non-static method used to get the underline rectangle of the game title instance.
+     * 
+     * @return  An instance of the Rectangle.java class, which will be the underline rectangle of the game title instance.
+     */
     public Rectangle getUnderlineRectangle() {
         return this.underlineRect;
     }
     
+    /*
+     * Public non-static method used to get the file for the font of the game title instance.
+     * 
+     * @return  An instance of the File.java class, which will be the file for the font of the game title instance.
+     */
     public File getFontFile() {
         return this.textFontFile;
     }
     
+    /*
+     * Public non-static method used to set the file for the font of the game title instance.
+     * 
+     * @param newFontFile A double, which will be the new file for the font of the game title instance.
+     */
     public void setFontFile(File newFontFile) {
         this.textFontFile = newFontFile;
     }
     
+    /*
+     * Public non-static method used to get a font of a specified size, from the file.
+     * 
+     * @param fontSize A double, which is the size of the desired font.
+     * @return  An instance of the Font.java class, which is the 
+     */
     public Font getFontFromFile(double fontSize) {
         Font fontFont = new Font(1); // It doesn't matter what size font I put, so I will default it to 1.
         try {
@@ -129,6 +263,11 @@ public class GameTitle {
         return fontFont;
     }
     
+    /*
+     * Public non-static method used to get a font of a specified size, from the file.
+     * 
+     * @param fontSize A double, which is the size of the desired font.
+     */
     public StackPane getTitle() {
         TextFlow tempTextFlow = new TextFlow();
         tempTextFlow.getChildren().addAll(
